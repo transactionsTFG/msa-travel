56ba6783fbfa93a6cf736431b7662355e9473dbc
danielfdez17 <daniel17dev@gmail.com>
2025-05-08 07:53:34 +0200

    fix: solved issue with queues annotations during deployment

--- src/main/java/integration/ResourceProducer.java
+++ src/main/java/integration/ResourceProducer.java
@@ -6,6 +6,10 @@ import javax.enterprise.inject.Produces;
 import javax.jms.ConnectionFactory;
 import javax.jms.Queue;
 
+import integration.producer.qualifiers.AirlineQueue;
+import integration.producer.qualifiers.HotelQueue;
+import integration.producer.qualifiers.TypeUserQueue;
+import integration.producer.qualifiers.UserQueue;
 import msa.commons.consts.JMSQueueNames;
 
 @ApplicationScoped
@@ -17,4 +21,23 @@ public class ResourceProducer {
     @Produces
     @Resource(lookup = JMSQueueNames.AGENCY_ORCHESTATOR_QUEUE)
     private Queue orchestratorQueue;
+
+    @Produces
+    @Resource(lookup = JMSQueueNames.AGENCY_TYPE_USER_SERVICE_QUEUE)
+    @TypeUserQueue
+    private Queue typeUserServiceQueue;
+    @Produces
+    @Resource(lookup = JMSQueueNames.AGENCY_USER_SERVICE_QUEUE)
+    @UserQueue
+    private Queue userServiceQueue;
+
+    @Produces
+    @Resource(lookup = JMSQueueNames.HOTEL_BOOKING_QUEUE)
+    @HotelQueue
+    private Queue hotelBookingQueue;
+
+    @Produces
+    @Resource(lookup = JMSQueueNames.AIRLINE_RESERVATION_QUEUE)
+    @AirlineQueue
+    private Queue reservationQueue;
 }
